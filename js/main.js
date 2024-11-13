001c9d2ab90a8e9751fc92e7e3e13456f2d851f1
Chandrakanth <chandrakanthkokkiripati1998@gmail.com>
2024-11-13 11:37:59 +0530

    c

--- js/main.js
+++ js/main.js
@@ -58,6 +58,37 @@
             }
         }
     });
+    $(".gallery-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:3
+            }
+        }
+    });
 
 
     // Facts counter
